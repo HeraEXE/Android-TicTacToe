96873075c82ecd20515f5f64cd72870d55ef7c46
Georgy Sevyan <g.sevyan@instadev.net>
2023-02-11 17:05:31 +0400

    Updated ViewScope

--- app/src/main/java/net/herasevyan/tictactoe/util/ViewScope.kt
+++ app/src/main/java/net/herasevyan/tictactoe/util/ViewScope.kt
@@ -1,8 +1,6 @@
 package net.herasevyan.tictactoe.util
 
-import android.util.Log
 import android.view.View
-import androidx.core.view.ViewCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -12,25 +10,15 @@ import java.io.Closeable
 import kotlin.coroutines.CoroutineContext
 
 private const val KEY_VIEW_SCOPE = R.id.view_scope
-private const val LOG_TAG = "ViewScope"
 
 val View.viewScope: CoroutineScope
     get() {
         getTag(KEY_VIEW_SCOPE)?.let {
-            if (it is CoroutineScope)
-                return it
-            else
-                Log.e(LOG_TAG, "check why the value of KEY_VIEW_SCOPE is ${it.javaClass.name}")
+            if (it is CoroutineScope) return it
         }
 
         val scope = CloseableCoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
         setTag(KEY_VIEW_SCOPE, scope)
-
-        if (!ViewCompat.isAttachedToWindow(this)) {
-            Log.w(LOG_TAG,
-                "Creating a CoroutineScope before ${javaClass.name} attaches to a window. " +
-                        "Coroutine jobs won't be canceled if the view has never been attached to a window.")
-        }
 
         addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
             override fun onViewAttachedToWindow(view: View) {}
